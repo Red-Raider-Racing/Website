252cfdb9a1a88e69d3c0899db12883816a05e12a
Carson-Spaniel <carsonspaniel@gmail.com>
2023-09-17 16:37:59 -0500

    more performance upgrades

--- RRRWebsite/settings.py
+++ RRRWebsite/settings.py
@@ -126,14 +126,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',  # Add this line
-]
 
 # Cache timeout for static files (in seconds)
 STATIC_CACHE_TTL = 2592000  # 30 days (30 * 24 * 60 * 60 seconds)
