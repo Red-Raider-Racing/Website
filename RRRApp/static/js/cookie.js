73c3add6bfc7abe538097bcc875235ae7d15a55d
Carson-Spaniel <carsonspaniel@gmail.com>
2023-10-04 21:33:30 -0500

    added cookies

---
+++ RRRApp/static/js/cookie.js
@@ -0,0 +1,48 @@
+//Cookies Popup Implementation for new users
+const cookieStorage = {
+    getItem: (key) => {
+        const cookies = document.cookie
+            .split(';')
+            .map(cookie => cookie.split('='))
+            .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {});
+        return cookies[key]
+    },
+    setItem: (key, value, daysToExpire = 30) => {
+        const date = new Date();
+        date.setTime(date.getTime() + (daysToExpire * 24 * 60 * 60 * 1000));
+        const expires = `expires=${date.toUTCString()}`;
+        const cookieOptions = `SameSite=None; Secure`;  // Include SameSite=None and Secure
+        document.cookie = `${key}=${value}; ${expires}; ${cookieOptions}; path=/`;
+    },
+};
+
+//Can change into any storage needed session, local, etc.
+const storageType = cookieStorage;
+const consentPropertyName = 'terms_accept';
+
+//Helper Functions to determine whether popup will be shown or not
+//References to local storage
+const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
+const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+
+window.onload = () => {
+    const consentPopup = document.getElementById('consent-popup');
+    if(consentPopup){
+        const acceptBtn = document.querySelector('.acceptA#accept');
+        const xBtn = document.querySelector('.buttonA#xaccept');
+
+        const acceptFn = event => {
+            saveToStorage(storageType);
+            consentPopup.classList.add('hidden');
+        };
+
+        acceptBtn.addEventListener('click', acceptFn);
+        xBtn.addEventListener('click', acceptFn);
+
+        if(shouldShowPopup()) {
+            setTimeout(() => {
+                consentPopup.classList.remove('hidden');
+            }, 2000);
+        }
+    }
+}; 
