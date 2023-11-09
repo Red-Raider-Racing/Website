a300fc0782acb0019b3acc0a84ba947f4b9c55ce
Carson-Spaniel <carsonspaniel@gmail.com>
2023-11-08 20:10:51 -0600

    documentation

--- RRRApp/functions/carShowReg.py
+++ RRRApp/functions/carShowReg.py
@@ -6,14 +6,13 @@ import requests
 import json
 from ..appSecrets import username, password
 
-url = 'https://sheetdb.io/api/v1/0v2vn3ko7qafp'
+url = 'https://sheetdb.io/api/v1/0v2vn3ko7qafp' #API endpoint
 
 headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
 }
 
-#Insert Row
 def insertRow(firstName, lastName, email, section):
     '''
     Function to insert details into the Google Sheet.
@@ -21,7 +20,7 @@ def insertRow(firstName, lastName, email, section):
     data = {
         'data': [
             {
-                'First Name': firstName.capitalize(),
+                'First Name': firstName.capitalize(), # For each of the data, match up the name with the names of the columns
                 'Last Name': lastName.capitalize(),
                 'Email': email,
                 'Section': section.capitalize(),
