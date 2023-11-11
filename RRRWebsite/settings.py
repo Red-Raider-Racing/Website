164df5dcbf6481ec13e9aaf038c65c5a7ca7c621
Carson-Spaniel <carsonspaniel@gmail.com>
2023-11-11 13:46:56 -0600

    having external db

--- RRRWebsite/settings.py
+++ RRRWebsite/settings.py
@@ -83,10 +83,30 @@ WSGI_APPLICATION = 'RRRWebsite.wsgi.application' # synchronous web requests
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+if DEBUG:
+    from .secrets.secret import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
+else:
+    DB_NAME = os.getenv('DB_NAME')
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_PORT = os.getenv('DB_PORT')
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
     }
 }
 
